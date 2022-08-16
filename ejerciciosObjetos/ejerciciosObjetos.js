b93e4cfaa734241d785a996d9e30c334bf40e6d8
Nico <110008941+ElN1ck08@users.noreply.github.com>
2022-08-15 23:11:09 -0500

    Apuntes y ejemplos libro

--- ejerciciosObjetos/ejerciciosObjetos.js
+++ ejerciciosObjetos/ejerciciosObjetos.js
@@ -37,4 +37,114 @@ var dvDn = numeros.denominador2 * numeros.numerador2
 console.log (`La division de los numeros es: ${dvNd}/${dvDn}`)
 
 
-// NO SIMPLIFICAAAA HPTA :c 
+// LIBRO PAG 150 - 152 
+
+
+// EJEMPLO 1
+let dog = { dogName: "JavaScript",
+            weight: 2.4,
+            color: "brown",
+            breed: "chihuahua"
+          };
+
+// EJEMPLO 2
+class ClassName {
+    constructor(prop1, prop2) {
+      this.prop1 = prop1;
+      this.prop2 = prop2;
+  }
+}
+let obj = new ClassName("arg1", "arg2");
+
+//EJEMPLO 3
+function Dog(dogName, weight, color, breed) {
+    this.dogName = dogName;
+    this.weight = weight;
+    this.color = color;
+    this.breed = breed;
+  }
+  let Dog = new Dog("Jacky", 30, "brown", "labrador");
+
+  // EJEMPLO 4
+  class Dog {
+    constructor(dogName, weight, color, breed) {
+      this.dogName = dogName;
+      this.weight = weight;
+      this.color = color;
+      this.breed = breed;
+  } }
+  let Dog = new Dog("JavaScript", 2.4, "brown", "chihuahua");
+
+  // EJEMPLO 5
+  class Person {
+    constructor(firstname, lastname) {
+      this.firstname = firstname;
+      this.lastname = lastname;
+    }
+  }
+  let p = new Person("Maaike", "van Putten");
+  console.log("Hi", p.firstname, p.lastname);
+
+
+  // EJEMPLO 6 
+  class Person {
+    constructor(firstname, lastname) {
+      this.firstname = firstname;
+      this.lastname = lastname;
+    }
+    greet() {
+      console.log("Hi there! I'm", this.firstname);
+  } 
+
+  // AGREGANDO UN COMPLEMENTO 
+    compliment(name, object) {
+    return "That's a wonderful " + object + ", " + name;
+}
+}
+    let S = new Person("Maaike", "van Putten");
+    S.greet();
+    let compliment = S.compliment("Harry", "hat");
+    console.log(compliment);
+
+ 
+    // A menudo, no es deseable proporcionar acceso directo a nuestras propiedades. Queremos que nuestra clase tenga el control de los valores de las propiedades por varias razones; tal vez queramos hacer una validación en una propiedad para asegurarnos de que tiene un cierto valor. Por ejemplo, imagina querer validar que una edad no es inferior a 18 años. Podemos lograr esto haciendo imposible el acceso directo a la propiedad desde fuera de la clase. Así es como se añaden propiedades a las que no se puede acceder desde el exterior. Les anteponemos un símbolo #:
+
+
+    // EJEMPLO 7
+    // Si quisiéramos asegurarnos de que solo podíamos crear objetos con nombres que comenzaran con una "M", podríamos modificar un poco nuestro constructor:
+
+    class Person {
+        #firstname;
+        #lastname;
+        constructor(firstname, lastname) {
+            if(firstname.startsWith("M")){
+              this.#firstname = firstname;
+            } else {
+              this.#firstname = "M" + firstname;
+            }
+            this.#lastname = lastname;
+          }
+    }
+    let b = new Person("kay", "Moon");
+
+    // Un prototipo es el mecanismo de JavaScript que permite tener objetos. Cuando no se especifica nada al crear una clase, los objetos heredan del prototipo Object.prototype. Esta es una clase de JavaScript integrada bastante compleja que podemos utilizar. No necesitamos ver cómo se implementa esto en JavaScript, ya que Podemos considerarlo el objeto base que siempre está encima del árbol de herencia y, por lo tanto, siempre está presente en nuestros objetos. Hay una propiedad prototipo disponible en todas las clases, y siempre se llama "prototipo". Podemos acceder a él de esta manera:
+    // EJEMPLO 8 
+
+    class Person {
+        constructor(firstname, lastname) {
+          this.firstname = firstname;
+          this.lastname = lastname;
+        }
+      greet() {
+          console.log("Hi there!");
+        }
+      }
+      Person.prototype.introduce = function () {
+        console.log("Hi, I'm", this.firstname);
+      };
+      
+      Person.prototype.favoriteColor = "green"
+
+      let c = new Person("Maria", "Saga");
+      console.log(p.favoriteColor);
+      p.introduce();
